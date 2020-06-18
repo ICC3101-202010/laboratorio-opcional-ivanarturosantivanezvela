a6d203b85c1820b0da73880afe99863540bb21b9
Ivan Arturo Santivañez Vela <Ivan@MacBook-Pro-de-Clementina-757.local>
2020-06-18 09:31:27 -0400

    primer commit

---
+++ Examen2019opcional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen2019opcional
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
